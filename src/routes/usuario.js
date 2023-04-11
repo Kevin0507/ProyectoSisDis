9a4d85cf708d1615830cac72121aec8d3e18ab50
Kevin0507 <kevin.hospino05@gmail.com>
2023-04-11 11:54:24 -0500

    Se agregó CRUD completo

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -20,4 +20,32 @@ router.get('/usuarios', (req,res)=>{
     .catch((error)=>res.json({message: error}));
 });
 
+//obtener un usuario
+router.get('/usuarios/:id', (req,res)=>{
+    const { id } = req.params;
+    usuarioSchema
+    .findById(id)
+    .then((data)=>res.json(data))
+    .catch((error)=>res.json({message: error}));
+});
+
+//actualizar un usuario
+router.put('/usuarios/:id', (req,res)=>{
+    const { id } = req.params;
+    const { name,age,email } = req.body;
+    usuarioSchema
+    .updateOne({ _id:id },{ $set: { name,age,email } } )
+    .then((data)=>res.json(data))
+    .catch((error)=>res.json({message: error}));
+});
+
+//eliminar un usuario
+router.delete('/usuarios/:id', (req,res)=>{
+    const { id } = req.params;
+    usuarioSchema
+    .findByIdAndRemove(id)
+    .then((data)=>res.json(data))
+    .catch((error)=>res.json({message: error}));
+});
+
 module.exports = router;
